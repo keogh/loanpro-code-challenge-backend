fe24612df6db624cb1b4331c189f04577de2038c
Isaac Zepeda <417939+keogh@users.noreply.github.com>
2024-03-28 16:14:08 -0700

    Add Postgres connection 2

--- codechallenge/settings.py
+++ codechallenge/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
     #     'PORT': os.environ.get('DB_PORT', None)
     # },
     'default': {
-        'ENGINE': os.environ.get('DB_ENGINE'),
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASSWORD'),
