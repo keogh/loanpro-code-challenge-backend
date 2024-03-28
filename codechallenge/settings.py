0f038468070b8886b909001bcc4a81ee0eadf1ce
Isaac Zepeda <417939+keogh@users.noreply.github.com>
2024-03-28 16:09:00 -0700

    Trying to connect to postgres

--- codechallenge/settings.py
+++ codechallenge/settings.py
@@ -79,13 +79,21 @@ WSGI_APPLICATION = 'codechallenge.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': os.environ.get('DB_ENGINE', 'django.db.backends.sqlite3'),
+    #     'NAME': os.environ.get('DB_NAME', BASE_DIR / 'db.sqlite3'),
+    #     'USER': os.environ.get('DB_USER', None),
+    #     'PASSWORD': os.environ.get('DB_PASSWORD', None),
+    #     'HOST': os.environ.get('DB_HOST', None),
+    #     'PORT': os.environ.get('DB_PORT', None)
+    # },
     'default': {
-        'ENGINE': os.environ.get('DB_ENGINE', 'django.db.backends.sqlite3'),
-        'NAME': os.environ.get('DB_NAME', BASE_DIR / 'db.sqlite3'),
-        'USER': os.environ.get('DB_USER', None),
-        'PASSWORD': os.environ.get('DB_PASSWORD', None),
-        'HOST': os.environ.get('DB_HOST', None),
-        'PORT': os.environ.get('DB_PORT', None)
+        'ENGINE': os.environ.get('DB_ENGINE'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT')
     }
 }
 
