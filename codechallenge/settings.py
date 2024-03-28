79656ead3be38c03772b09f88234c8a2e3e2a9a0
Isaac Zepeda <417939+keogh@users.noreply.github.com>
2024-03-28 15:58:58 -0700

    Add Postgres connection

--- codechallenge/settings.py
+++ codechallenge/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'codechallenge.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.environ.get('DB_ENGINE', 'django.db.backends.sqlite3'),
+        'NAME': os.environ.get('DB_NAME', BASE_DIR / 'db.sqlite3'),
+        'USER': os.environ.get('DB_USER', None),
+        'PASSWORD': os.environ.get('DB_PASSWORD', None),
+        'HOST': os.environ.get('DB_HOST', None),
+        'PORT': os.environ.get('DB_PORT', None)
     }
 }
 
