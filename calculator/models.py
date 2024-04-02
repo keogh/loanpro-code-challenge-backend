5815fac1f9bdf846295318c8547a9bd843e5089e
Isaac Zepeda <417939+keogh@users.noreply.github.com>
2024-04-02 11:18:56 -0700

    Add Record model

--- calculator/models.py
+++ calculator/models.py
@@ -1,6 +1,20 @@
 from django.db import models
+from django.conf import settings
+from django.utils import timezone
 
 
 class Operation(models.Model):
     type = models.CharField(max_length=100, unique=True)
     cost = models.PositiveIntegerField(default=0, null=False)
+
+
+class Record(models.Model):
+    operation = models.ForeignKey('calculator.Operation', on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    amount = models.IntegerField(null=False)
+    user_balance = models.IntegerField(null=False)
+    operation_response = models.CharField(max_length=100)
+    created_at = models.DateTimeField(default=timezone.now, null=False)
+
+    def __str__(self):
+        return f"Record {self.pk} - User: {self.user.username} - Operation: {self.operation.type}"
