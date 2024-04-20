13339e341f4225cf525be35190a48bfaaa9853a3
Isaac Zepeda <417939+keogh@users.noreply.github.com>
2024-04-19 18:41:08 -0700

    Another cors allow

--- codechallenge/settings.py
+++ codechallenge/settings.py
@@ -151,6 +151,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # CORS_ALLOW_ALL_ORIGINS = True  # Not recommended for production
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
+    'http://127.0.0.1:8081',
     # 'https://yourproductionfrontend.com',
     # 'https://keogh.github.io',
     'https://loanpro-django-api.apps.bitacoraml.com',
